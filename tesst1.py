0e28b5ac0874867ef96f825b96c7c3002832aab5
grishapanc-jpg <grishapanc@gmail.com>
2025-09-13 11:52:50 +0300

    sxascascwec

--- tesst1.py
+++ tesst1.py
@@ -1,1 +1,35 @@
-print ('hello world')
+import random
+from tkinter.font import names
+
+
+class Student:
+    def __init__(self):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_study(self):
+        print('Time to study')
+        self.progress +=0.12
+        self.gladness -=3
+
+    def to_sleep(self):
+        print('Time to sleep')
+        self.gladness +=3
+
+
+    def to_chill(self):
+        print('Rest time!')
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def to_chill(self):
+
+        print('Cast out...')
+        self.gladness += 5
+        self.progress -= 0.1
+
+
+
+

0dce39952f36610eb92fb5f78164939a5331f051
grishapanc-jpg <grishapanc@gmail.com>
2025-09-20 10:03:15 +0300

    intro & sys

--- p5_6.py
+++ p5_6.py
@@ -3,3 +3,4 @@ import sys
 print(sys.executable)
 print(sys.version)
 print(sys.platform)
+print(sys.argv)
---
+++ p5_7.py
@@ -0,0 +1,7 @@
+import sys
+import requests
+import random
+
+for module_name, module_path in sys.modules.items():
+    print(module_name, module_path)
+
